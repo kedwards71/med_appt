fcc4cbe5413560c31bf3cdf76a8bc0fe46249a48
Khaemon <khaemonedwards@yahoo.com>
2026-06-11 09:11:26 -0400

    Updated FindDoctorSearch

--- src/Components/FindDoctorSearch/FindDoctorSearch.jsx
+++ src/Components/FindDoctorSearch/FindDoctorSearch.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 import './FindDoctorSearch.css'
-
+import {useNavigate} from 'react-router-dom';
 
 const FindDoctorSearch = () => {
     const [search,setSearch] = useState('');
+    const [doctorResultHidden, setDoctorResultHidden] = useState(true);
+    const navigate = useNavigate();
+
+
     return(
-        <>
-            <div className="search-container">
-                <img src="../assets/search.svg" alt="search-button" />
-                <input type="search" value={search} name="search" id="search" onChange={(e)=>setSearch(e.target.value)}/>
-            </div>
-        </>
+        <div className="container">
+            <center>
+                <div className="search-container">
+                    <div className="search-box">
+                        <input type="search" name="search-bar" id="search-bar" placeholder="..." />
+                    </div>
+                </div>
+            </center>
+        </div>
     )
 }
 
