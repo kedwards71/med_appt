778f1e2955e9e79d1374d2f842cd421205345358
Khaemon Edwards <khaemonedwards@yahoo.com>
2026-06-11 20:14:12 -0400

    Updated

---
+++ src/Components/DoctorCard/DoctorCard.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+
+const DoctorCard = ({name,speciality, ratings, profilePic}) =>{
+    return(
+        <>
+        </>
+    )
+};
+
+export default DoctorCard;
