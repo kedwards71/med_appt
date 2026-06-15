fcaba136f9c6db79303ef21fae4a87da6737b819
Khaemon <khaemonedwards@yahoo.com>
2026-06-15 16:31:36 -0400

    Styling for Report

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:orD2n4zx7HVqxXFiiO4u9UEf@172.21.54.80:27017";
+const mongoURI =  "mongodb://root:yteNCyGOaIHgbYTjAU1K9Mm3@172.21.64.80:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
--- src/Components/ReportsLayout/ReportsLayout.jsx
+++ src/Components/ReportsLayout/ReportsLayout.jsx
@@ -4,6 +4,25 @@ import './ReportsLayout.css';
 const ReportsLayout = () => {
     return (
         <>
+            <h1>Reports</h1>
+            <div className="report-container">
+                <table className="report-table">
+                    <tr className="report-headers">
+                    <th>Serial Number</th>
+                    <th>Doctor Name</th>
+                    <th>Doctor Speciality</th>
+                    <th>View Report</th>
+                    <th>Download Report</th>
+                    </tr>
+                    <tr className="report-data">
+                        <td>DoctorIndex</td>
+                        <td>DoctorName</td>
+                        <td>DoctorSpeciality</td>
+                        <td><button className="btn btn-primary">View Report</button></td>
+                        <td><button className="btn btn-primary">Download Report</button></td>
+                    </tr>
+                </table>
+            </div>
         </>
     )
 }
